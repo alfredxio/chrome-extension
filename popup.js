47d773d58243dc96d1c185507b9b8288ebb5835d
Rohit Raj <mr.rajarohit9431@gmail.com>
2024-06-01 19:45:40 +0530

    enable button changed to toggle

--- popup.js
+++ popup.js
@@ -1,32 +1,45 @@
-document.getElementById("enable").addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("toggleButton");
+
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentSite = new URL(tabs[0].url).hostname;
+
     chrome.storage.local.get(["activatedSites"], (result) => {
-      let activatedSites = result.activatedSites || [];
-      if (!activatedSites.includes(currentSite)) {
-        activatedSites.push(currentSite);
-        chrome.storage.local.set({ activatedSites }, () => {
-          chrome.scripting.executeScript({
-            target: { tabId: tabs[0].id },
-            files: ["content.js"],
-          });
-        });
+      const activatedSites = result.activatedSites || [];
+      if (activatedSites.includes(currentSite)) {
+        toggleButton.classList.add("enabled");
       }
     });
   });
-});
 
-document.getElementById("disable").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const currentSite = new URL(tabs[0].url).hostname;
-    chrome.storage.local.get(["activatedSites"], (result) => {
-      let activatedSites = result.activatedSites || [];
-      activatedSites = activatedSites.filter((site) => site !== currentSite);
-      chrome.storage.local.set({ activatedSites }, () => {
-        chrome.scripting.executeScript({
-          target: { tabId: tabs[0].id },
-          function: disableScript,
-        });
+  toggleButton.addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentSite = new URL(tabs[0].url).hostname;
+
+      chrome.storage.local.get(["activatedSites"], (result) => {
+        let activatedSites = result.activatedSites || [];
+
+        if (toggleButton.classList.contains("enabled")) {
+          activatedSites = activatedSites.filter(
+            (site) => site !== currentSite
+          );
+          chrome.storage.local.set({ activatedSites }, () => {
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              function: disableScript,
+            });
+            toggleButton.classList.remove("enabled");
+          });
+        } else {
+          activatedSites.push(currentSite);
+          chrome.storage.local.set({ activatedSites }, () => {
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              files: ["content.js"],
+            });
+            toggleButton.classList.add("enabled");
+          });
+        }
       });
     });
   });
